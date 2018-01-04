2be189e4fc8d7310b7a051283c8247d0326689f2
AngelaF <ADFrys@users.noreply.github.com>
2018-01-04 14:56:32 -0600

    Create answer object in JS.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,27 @@
+// when page loads, start timer for 120 seconds (setTimeout or setInterval)
+window.onload = function() {
+setTimeout(oneTwentyS, 120000);
+};
+
+// countdown remaining seconds using a function
+// load new page that displays correct answers, wrong answers and unanswered questions
+function oneTwentyS() {
+
+}
+//
 
 // create object that stores correct answers
 var CorrectAnswers = {
-	q1: "",
-	q2: "",
+	q1: "7x",
+	q2: "10v",
+	q3: "100v",
+	q4: "red",
+	q5: "400x",
+	q6: "longTail",
+	q7: "9s",
+	q8: "43y",
 };
 
-// if user picks correct answer = 1pt correct answer
-// else = incorrect answer
+// if user picks correct answer, count towards correct answer
+// if user picks incorrect answer, count towards incorrect answer
 

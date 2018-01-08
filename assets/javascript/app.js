a4f26bba85389a063832e071be8727014a0f3e37
AngelaF <ADFrys@users.noreply.github.com>
2018-01-08 12:51:29 -0600

    Add remaining questions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,35 +10,42 @@ $(document).ready(function() {
   q7: "9s",
   q8: "43y",
   };
+// create an empty object 
+// answer chosen with same notation (for each question id)
+// chosen[name] = question
+// answers[q1] = val
 
-  var countDownNumber = 5;
+  var countDownNumber = 120;
   var time;
-  var answerQ1;
+  var question;
+  var answerQ;
   var numberCorrect = 0;
   var numberIncorrect = 0;
   var unansweredQ = 8;
 
   function check(answer, name) {
+    for (var i=0; i<9; i++) {
+    question = "q"+[i];
+  }
     if (answer == correctAnswers[name]) {
-      alert("hello");
       numberCorrect++;
       unansweredQ--;
-      console.log(numberCorrect);
     }
     else if (answer !== correctAnswers[name]) {
       unansweredQ--;
       numberIncorrect++;
+      console.log("name correcta" + correctAnswers[name]);
     } 
   }
 
   function stop() {
 	clearInterval(time);
+  check(answerQ, question);
 	$(".remaining-time").text(" " + countDownNumber + " ");
 	$(".body").empty();
 	$(".body").html("<h1>Thank you for playing pet trivia!</h1>" + "<p>" + "Number Correct: " + numberCorrect + "</p>");
 	$(".body").append("<p>" + "Number Incorrect: " + numberIncorrect + "</p>");
 	$(".body").append("Unanswered questions: " + unansweredQ + " ");
-	console.log(numberCorrect + "check");
   }
 
   function oneTwentyS() {
@@ -52,21 +59,21 @@ $(document).ready(function() {
   $(".remaining-time").append(" " + countDownNumber + " ");
   time = setInterval(oneTwentyS, 1000);
 
-  $("input[type=radio][name=q1]").on("click", function() {
-    answerQ1 = $("input[type=radio][name=q1]:checked").val();
-    console.log(answerQ1);
-    check(answerQ1, "q1");
+//create as an object
+  $("input[type=radio]").on("click", function() {
+    answerQ = $("input[type=radio]:checked").val();
+    console.log("answerQ " + answerQ);
+    console.log(question + "question")
   });
+
 
   $("form").submit(function quizDone() {
     event.preventDefault();
+    check(answerQ, question);
     $(".body").empty();
     $(".body").html("<h1>Thank you for playing pet trivia!</h1>" + "<p>" + "Number Correct: " + numberCorrect + "</p>");
     $(".body").append("<p>" + "Number Incorrect: " + numberIncorrect + "</p>");
     $(".body").append("Unanswered questions: " + unansweredQ + " ");
-    console.log(numberCorrect + "check");
   });
 });
-
-// userAnswer = (correctAnswers[i].$('input[name=q'+i+']:checked')||{}).value;
 
